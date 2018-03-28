54cb3ecda6e27675488412ac75f21a5218c8a99e
alondrew <andellanoalonso@gmail.com>
2018-03-28 19:23:44 +0200

    :white_check_mark: Add some test heads for transaction interface

---
+++ transaction_test.go
@@ -0,0 +1,46 @@
+package ncoin_wallet
+
+import (
+    "testing"
+)
+
+
+func TestSignTransactionFull(t *testing.T) {
+
+
+}
+
+func TestSignTransactionEmpty(t *testing.T) {
+
+
+}
+
+func TestFailSignByChange(t *testing.T) {
+
+
+}
+
+func TestFailSignByBadCredentials(t *testing.T) {
+
+
+}
+
+func TestTransactionStringifyEmpty(t *testing.T) {
+
+
+}
+
+func TestTransactionStringifyFull(t *testing.T) {
+
+
+}
+
+func TestTransactionStringifyOneElement(t *testing.T) {
+
+
+}
+
+func TestTransactionStringifyWithoutOneElement(t *testing.T) {
+
+
+}
