e31ee2a3c909f1174185b5f6b85280e07924ce4b
alondrew <andellanoalonso@gmail.com>
2018-04-07 19:10:02 +0200

    :sparkles: Implements the GenerateKeys function

--- utils.go
+++ utils.go
@@ -3,17 +3,23 @@ package ncoin_wallet
 import (
 	"crypto/sha256"
 	"crypto/rsa"
+    "crypto/rand"
 )
 
-func CalculateGenericHash(string s) []byte{
+func CalculateGenericHash(s string) []byte{
 	h := sha256.New()
 	h.Write([]byte(s))
 	return h.Sum(nil)
 }
 
 
-func GenerateKeys() *rsa.PrivateKey, *rsa.PublicKey {
-
+func GenerateKeys() (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privateKey, err := rsa.GenerateKey( rand.Reader, 2048)
+    if err != nil {
+        return nil, nil, err
+    }
+    var publicKey = privateKey.PublicKey
+	return privateKey, &publicKey, err
 }
 
 
