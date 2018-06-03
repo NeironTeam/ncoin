465539d65a9d04cf8a0772642ea5c7b5c915b239
Lucas Menendez <hi@lucasmenendez.me>
2018-06-03 02:05:14 +0200

    from/to json transaction

--- internal/utils.go
+++ internal/utils.go
@@ -6,21 +6,21 @@ import (
 )
 
 //CalculateGenericHash return the SHA256 hash of String s.
-func CalculateGenericHash(s string) []byte{
+func CalculateGenericHash(s string) []byte {
 	h := sha256.New()
 	h.Write([]byte(s))
 	return h.Sum(nil)
 }
 
 // TODO (Ventura): Revisar, puede no funcionar, mirar las funciones ancestrales
-func ProcessSHA256(data *[]byte){
+func ProcessSHA256(data *[]byte) {
 	h := sha256.New()
 	h.Write(data)
 	data = h.Sum(nil)
 }
 
 // TODO (Ventura): Revisar, puede no funcionar, mirar las funciones ancestrales
-func ProcessRIPEMD160(data *[]byte){
+func ProcessRIPEMD160(data *[]byte) {
 	h := ripemd160.New()
 	h.Write(data)
 	data = h.Sum(nil)
