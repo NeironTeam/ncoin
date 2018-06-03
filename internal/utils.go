72922870d7f49249a88f7da42bea4a97f040fa95
Daniel Gak Anagrov <dani_gak@hotmail.com>
2018-06-03 02:42:57 +0200

    :speech_balloon: small merge

--- internal/utils.go
+++ internal/utils.go
@@ -1,27 +1,36 @@
-package ncoin_wallet
+package internal
 
 import (
 	"crypto/sha256"
 	"golang.org/x/crypto/ripemd160"
+	"os"
 )
 
 //CalculateGenericHash return the SHA256 hash of String s.
-func CalculateGenericHash(s string) []byte{
+func CalculateGenericHash(s string) []byte {
 	h := sha256.New()
 	h.Write([]byte(s))
 	return h.Sum(nil)
 }
 
+func GetEnv(envVarName string, defVarValue string) (env string) {
+	if env = os.Getenv(envVarName); env == "" {
+		env = defVarValue
+	}
+	return
+}
+
+
 // TODO (Ventura): Revisar, puede no funcionar, mirar las funciones ancestrales
-func ProcessSHA256(data *[]byte){
+func ProcessSHA256(data []byte) []byte {
 	h := sha256.New()
 	h.Write(data)
-	data = h.Sum(nil)
+	return h.Sum(nil)
 }
 
 // TODO (Ventura): Revisar, puede no funcionar, mirar las funciones ancestrales
-func ProcessRIPEMD160(data *[]byte){
+func ProcessRIPEMD160(data []byte) []byte {
 	h := ripemd160.New()
 	h.Write(data)
-	data = h.Sum(nil)
+	return h.Sum(nil)
 }
